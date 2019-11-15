676931908223675584fbbcd08e1785d84d4527bc
root <2105497852@qq.com>
2019-11-15 15:48:42 +0800

    AAA

---
+++ src/main/java/com/aaa/c/A.java
@@ -0,0 +1,19 @@
+package com.aaa.c;
+
+import com.aaa.service.Demo;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class A {
+    @Reference
+    private Demo demo;
+
+    @RequestMapping("/a")
+    @ResponseBody
+    public String a(){
+        return demo.aa();
+    }
+}
---
+++ src/main/java/com/aaa/service/Demo.java
@@ -0,0 +1,5 @@
+package com.aaa.service;
+
+public interface Demo {
+    String aa();
+}
